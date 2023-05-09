dca873ff80ed6a6dfda80a1ecc44c3aa084bbb21
naka3600 <n.yoshiaki7636.icloud.com>
2023-05-10 06:31:16 +0900

    first

---
+++ ResidentApp/ResidentApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Starting myapp...");
+        while (true)
+        {
+            Console.WriteLine("myapp is running...");
+            Thread.Sleep(5000);
+        }
+    }
+}
